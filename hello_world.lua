862853e17bde00d9be074226ecdce6e2e491df2a
Isaac Goss <isaacgoss14750@gmail.com>
2023-12-08 17:09:25 -0500

    Adds doc comment on how to run hello_world script in ComputerCraft

--- hello_world.lua
+++ hello_world.lua
@@ -1,3 +1,11 @@
 
+--[[
+    Run this script directly in a ComputerCraft terminal with the following:
+
+    > wget run https://raw.githubusercontent.com/iggy14750/computercraft/master/hello_world.lua
+
+    This URL can be modified to enable the use of different repositories
+    or scripts.
+--]]
 print("Hello, ComputerCraft!")
 
